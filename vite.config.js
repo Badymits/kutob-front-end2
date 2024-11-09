166b498fc9f90b6c5f9ce2cafb6db92ef59143d6
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-09 17:58:16 +0800

    added vite imagemin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    viteImagemin({
+      gifsicle: { 
+        optimizationLevel: 3 
+      },
+      optipng: { 
+        optimizationLevel: 7 
+      },
+      mozjpeg: { 
+        quality: 80 
+      },
+      pngquant: { 
+        quality: [0.6, 0.8] 
+      },
+      svgo: {
+        plugins: [
+          { removeViewBox: false },
+          { removeEmptyAttrs: true },
+        ],
+      },
+    }),
+  ],
 })
